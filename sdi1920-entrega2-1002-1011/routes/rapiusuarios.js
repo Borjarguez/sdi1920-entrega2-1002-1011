2d7d62cda634816b0785d6786cc272ca44503ace
Borja <uo258643@uniovi.es>
2020-05-05 00:33:20 +0200

    Cambios menores

--- sdi1920-entrega2-1002-1011/routes/rapiusuarios.js
+++ sdi1920-entrega2-1002-1011/routes/rapiusuarios.js
@@ -1,134 +1,107 @@
 module.exports = function (app, swig, gestorBD) {
 
-        app.post("/api/autenticar", function (req, res) {
-            let seguro = app.get("crypto").createHmac('sha256', app.get('clave')).update(req.body.password).digest('hex');
+    app.post("/api/autenticar", function (req, res) {
+        let seguro = app.get("crypto").createHmac('sha256', app.get('clave')).update(req.body.password).digest('hex');
 
-            let criterio = {
-                email: req.body.email,
-                password: seguro
-            };
+        let criterio = {
+            email: req.body.email,
+            password: seguro
+        };
 
-            gestorBD.obtenerUsuarios(criterio, function (usuarios) {
-                if (usuarios == null || usuarios.length == 0) {
-                    res.status(401);
-                    res.json({
-                        autenticado: false
-                    })
-                } else {
-                    var token = app.get('jwt').sign({
-                        usuario: criterio.email,
-                        tiempo: Date.now() / 1000
-                    }, "secreto");
-                    res.status(200);
-                    res.json({
-                        autenticado: true,
-                        token: token
-                    })
-                }
-            })
+        gestorBD.obtenerUsuarios(criterio, function (usuarios) {
+            if (usuarios == null || usuarios.length == 0) {
+                res.status(401);
+                res.json({
+                    autenticado: false
+                })
+            } else {
+                var token = app.get('jwt').sign({
+                    usuario: criterio.email,
+                    tiempo: Date.now() / 1000
+                }, "secreto");
+                res.status(200);
+                res.json({
+                    autenticado: true,
+                    token: token
+                })
+            }
+        })
+    });
+
+    app.get("/api/friend", function (req, res) {
+        let criteria = {
+            $or: [{
+                "sender": req.session.usuario,
+                "accepted": true
+            } || {
+                "receiver": req.session.usuario,
+                "accepted": true
+            }]
+        };
+
+        gestorBD.obtenerAmigos(criteria, function (amigos) {
+            if (amigos == null) {
+                res.status(500);
+                res.json({
+                    error: "se ha producido un error"
+                });
+            } else {
+                res.status(200);
+                res.send(JSON.stringify(amigos));
+            }
         });
+    });
 
-        app.get("/api/friend", function (req, res) {
-            let criteria = {
-                $or: [{
-                    "sender": req.session.usuario,
+    app.put("/api/chat/:id", function (req, res) {
+        var criteria = {
+            "$or": [{
+                "receiver": res.usuario
+            }, {
+                "sender": res.usuario
+            }]
+        };
+
+        gestorBD.obtenerUsuarios({
+            "_id": gestorBD.mongo.ObjectID(req.params.id)
+        }, function (usuario) {
+            if (usuario == null || usuario[0] == null) {
+                res.status(500);
+                res.json({
+                    error: "se ha producido un error"
+                });
+            } else {
+                gestorBD.obtenerAmigos({
+                    "receiver": usuario[0].email,
                     "accepted": true
-                } || {
-                    "receiver": req.session.usuario,
-                    "accepted": true
-                }]
-            };
-
-            gestorBD.obtenerAmigos(criteria, function (amigos) {
-                if (amigos == null) {
-                    res.status(500);
-                    res.json({
-                        error: "se ha producido un error"
-                    });
-                } else {
-                    res.status(200);
-                    res.send(JSON.stringify(amigos));
-                }
-            });
-        });
-
-        app.put("/api/chat/:id", function (req, res) {
-            var criteria = {
-                "$or": [{
-                    "receiver": res.usuario
-                }, {
-                    "sender": res.usuario
-                }]
-            };
-
-            gestorBD.obtenerUsuarios({
-                "_id": gestorBD.mongo.ObjectID(req.params.id)
-            }, function (usuario) {
-                if (usuario == null || usuario[0] == null) {
-                    res.status(500);
-                    res.json({
-                        error: "se ha producido un error"
-                    });
-                } else {
-                    gestorBD.obtenerAmigos({
-                        "receiver": usuario[0].email,
-                        "accepted": true
-                    }, function (amigo) {
-                        if (amigo == null || amigo[0] == null) {
-                            res.status(500);
-                            res.json({
-                                error: "se ha producido un error"
-                            });
-                        } else {
-                            gestorBD.obtenerConversaciones(criteria, function (conversaciones) {
-                                if (conversaciones == null) {
-                                    res.status(500);
-                                    res.json({
-                                        error: "se ha producido un error"
-                                    });
-                                } else {
-                                    if (conversaciones[0] == null) {
-                                        var conversacion = {
-                                            "sender": res.usuario,
-                                            "receiver": amigo[0],
-                                            "mensajes": [{
-                                                "_id": gestorBD.mongo.ObjectID(),
-                                                "sender": res.usuario,
-                                                "receiver": amigo[0],
-                                                "texto": req.body.text,
-                                                "read": false
-                                            }]
-                                        };
-
-                                        gestorBD.insertarConversacion(conversacion,
-                                            function (conversaciones) {
-                                                if (conversaciones == null) {
-                                                    res.status(500);
-                                                    res.json({
-                                                        error: "se ha producido un error"
-                                                    });
-                                                } else {
-                                                    res.status(201);
-                                                    res.json({
-                                                        mensaje: "conversacion iniciada",
-                                                    })
-                                                }
-                                            });
-                                    } else {
-                                        var mensaje = {
+                }, function (amigo) {
+                    if (amigo == null || amigo[0] == null) {
+                        res.status(500);
+                        res.json({
+                            error: "se ha producido un error"
+                        });
+                    } else {
+                        gestorBD.obtenerConversaciones(criteria, function (conversaciones) {
+                            if (conversaciones == null) {
+                                res.status(500);
+                                res.json({
+                                    error: "se ha producido un error"
+                                });
+                            } else {
+                                if (conversaciones[0] == null) {
+                                    var conversacion = {
+                                        "sender": res.usuario,
+                                        "receiver": amigo[0],
+                                        "mensajes": [{
                                             "_id": gestorBD.mongo.ObjectID(),
                                             "sender": res.usuario,
                                             "receiver": amigo[0],
-                                            "date": new Date(),
                                             "texto": req.body.text,
                                             "read": false
-                                        };
+                                        }]
+                                    };
 
-                                        gestorBD.insertarMensaje(criterio, {
-                                            "$push": {
-                                                "mensajes": mensaje
-                                            }
-                                        }, function (conversaciones) {
+                                    gestorBD.insertarConversacion(conversacion,
+                                        function (conversaciones) {
                                             if (conversaciones == null) {
                                                 res.status(500);
                                                 res.json({
@@ -137,40 +110,63 @@ module.exports = function (app, swig, gestorBD) {
                                             } else {
                                                 res.status(201);
                                                 res.json({
-                                                    mensaje: "mensaje enviado",
+                                                    mensaje: "conversacion iniciada",
                                                 })
                                             }
                                         });
-                                    }
-                                }
-                            });
-                        }
-                    });
-                }
-            });
-        });
+                                } else {
+                                    var mensaje = {
+                                        "_id": gestorBD.mongo.ObjectID(),
+                                        "sender": res.usuario,
+                                        "receiver": amigo[0],
+                                        "date": new Date(),
+                                        "texto": req.body.text,
+                                        "read": false
+                                    };
 
-        app.get("/api/chat", function (req, res) {
-            var criterio = {
-                "$or": [{
-                        "sender": res.usuario
-                    },
-                    {
-                        "receiver": res.usuario
+                                    gestorBD.insertarMensaje(criterio, {
+                                        "$push": {
+                                            "mensajes": mensaje
+                                        }
+                                    }, function (conversaciones) {
+                                        if (conversaciones == null) {
+                                            res.status(500);
+                                            res.json({
+                                                error: "se ha producido un error"
+                                            });
+                                        } else {
+                                            res.status(201);
+                                            res.json({
+                                                mensaje: "mensaje enviado",
+                                            })
+                                        }
+                                    });
+                                }
+                            }
+                        });
                     }
-                ]
-            };
-            gestorBD.obtenerConversaciones(criterio, function (mensajes) {
-                if (mensajes == null) {
-                    res.status(403);
-                    res.json({
-                        acceso: false,
-                        error: 'Acceso denegado'
-                    });
-                    return;
-                } else {
-                    res.status(200);
-                    res.send(JSON.stringify(mensajes));
-                }
-            });
+                });
+            }
         });
+    });
+
+    app.get("/api/chat", function (req, res) {
+        var criterio = {
+            "$or": [{"sender": res.usuario},{"receiver": res.usuario}]
+        };
+
+        gestorBD.obtenerConversaciones(criterio, function (mensajes) {
+            if (mensajes == null) {
+                res.status(403);
+                res.json({
+                    acceso: false,
+                    error: 'Acceso denegado'
+                });
+                return;
+            } else {
+                res.status(200);
+                res.send(JSON.stringify(mensajes));
+            }
+        });
+    });
+};
